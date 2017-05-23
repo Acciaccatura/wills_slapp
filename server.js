73aa36602b0965cb77ec962e15ae16ceab4f04cd
Chia <wchia@aus2m-wchia.rws.ad.ea.com>
2017-05-23 12:01:58 -0500

    test

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
+const port = process.env.PORT || 3000
+
 var slapp = Slapp({
   convo_store: ConvoStore(),
   context: Context()
@@ -14,7 +16,23 @@ slapp.message('eyy', ['direct_message'], (msg, text) => {
   msg.say(':o', (err, data) => {})
 })
 
+slapp.event('message.channels', (msg) => {
+  if (msg.meta.app_token) {
+    msg.say('howdy', (err, data) => {
+      if (err) {
+        console.log(err)
+      }
+     })
+  } else {
+    msg.say('aww...')
+  }
+})
+
 var app = express()
 slapp.attachToExpress(app)
 
-app.listen(process.env.PORT || 3000)
+
+
+app.listen(port, () => {
+  console.log('Brunch was delicious, and now we\'re going to go see a movie, like we always do. What are you thinking this time, partner, ' + port + '?')
+})
