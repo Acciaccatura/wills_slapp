57d128f60d89b23eb4e7a659ba1b164a6275bad3
Chia <wchia@aus2m-wchia.rws.ad.ea.com>
2017-05-23 15:09:36 -0500

    work

--- server.js
+++ server.js
@@ -4,35 +4,54 @@ const express = require('express')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
+const http = require('http')
 
 const port = process.env.PORT || 3000
 
+//test for translation API
+const translate_options = {
+  'host': 'www.transltr.org',
+  'path': '/api/translate/',
+  'method': 'POST',
+  'headers': {
+    'Content-Type': 'application/json'
+  }
+}
+//note to self, modularize this if you ever consider making this something
+function translate(phrase, from, to, callback) {
+  let string = {
+    'text': phrase,
+    'from': from,
+    'to': to
+  }
+  let req = http.request(translate_options, (res) => {
+    res.setEncoding('utf-8')
+    res.on('data', (data) => {
+      callback(data)
+    })
+  })
+  req.write(JSON.stringify(string))
+  req.end()
+}
+
+//Slapp starts here!!!!
 var slapp = Slapp({
   convo_store: ConvoStore(),
   context: Context()
 })
 
-slapp.message('eyy', ['direct_message'], (msg, text) => {
-  msg.say(':o', (err, data) => {})
-})
-
-slapp.event('message', (msg) => {
+slapp.message(' (.*)', ['ambient'], (msg, text, match1) => {
   if (msg.meta.app_token) {
-    msg.say('howdy', (err, data) => {
-      if (err) {
-        console.log(err)
-      }
-     })
-  } else {
-    msg.say('aww...')
+    console.log(msg.body)
   }
 })
 
+slapp.event()
+
+//initialize express!!!
 var app = express()
 slapp.attachToExpress(app)
 
-
-
 app.listen(port, () => {
-  console.log('Brunch was delicious, and now we\'re going to go see a movie, like we always do. What are you thinking this time, partner, ' + port + '?')
+  console.log('Brunch was delicious, and now we usually see a movie, like we always do. What are you thinking this time, partner, ' + port + '?')
 })
